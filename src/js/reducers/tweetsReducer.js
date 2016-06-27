09d482061f74f7a5844d06b52201085f08cc741c
Yeiber Cano <yeibercano@gmail.com>
2016-06-27 15:08:52 -0700

    add spread operator to reducer

--- src/js/reducers/tweetsReducer.js
+++ src/js/reducers/tweetsReducer.js
@@ -1,3 +1,9 @@
 export const tweetsReducer = (state=[], action) => {
+  switch(action.type) {
+    case "CHANGE_MESSAGE": {
+        state = [...state, message: action.payload ]
+      }
+      break;
+  }
   return state;
 };
