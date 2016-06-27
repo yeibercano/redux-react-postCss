8d4a4c3ad86dcd393b8151e3a29e94fe7c2b7639
Yeiber Cano <yeibercano@gmail.com>
2016-06-27 13:37:42 -0700

    set app container

---
+++ src/js/containers/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Main   from './components/Main';
+import Footer from './components/Footer';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+       <Header/>
+       <Main />
+       <Footer />
+      </div>
+    )
+  }
+}
