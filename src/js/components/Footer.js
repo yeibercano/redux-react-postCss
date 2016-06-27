5cabd95bb40365613801ee26da995b39d3bba3f3
Yeiber Cano <yeibercano@gmail.com>
2016-06-27 13:00:37 -0700

    create header and footer

---
+++ src/js/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+     <footer>
+       <h1>I'm the footer</h1>
+     <footer/>
+    )
+  }
+}
---
+++ src/js/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+     <header>
+       <h1>I'm the header</h1>
+     <header/>
+    )
+  }
+}
