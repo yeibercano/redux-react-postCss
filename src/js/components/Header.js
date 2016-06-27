219e50f2cf5328a709d43a1374abb725332c5d94
Yeiber Cano <yeibercano@gmail.com>
2016-06-27 14:59:21 -0700

    fix / position

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react';
 
 export default class Header extends Component {
   render() {
     return (
      <header>
        <h1>I'm the header</h1>
-     <header/>
+     </header>
     )
   }
 }
