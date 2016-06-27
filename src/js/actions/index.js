a0068a6439117ccdbad94ebbbedca39324de649d
Yeiber Cano <yeibercano@gmail.com>
2016-06-27 13:00:06 -0700

    set actions to its own folder

---
+++ src/js/actions/index.js
@@ -0,0 +1,13 @@
+import * as types from '../constants/ActionTypes'
+
+export function getChangeName() {
+  return {
+    type: types.CHANGE_NAME,
+  }
+}
+
+export function getChangeAge() {
+  return {
+    type: types.CHANGE_AGE,
+  }
+}
