3395cafc12f02050f54df31169adea6e2f07f8b2
Yeiber Cano <yeibercano@gmail.com>
2016-06-27 14:58:53 -0700

    fix routes

--- src/js/containers/App.js
+++ src/js/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import Main   from './components/Main';
-import Footer from './components/Footer';
+import Header from '../components/Header';
+import Main   from './Main';
+import Footer from '../components/Footer';
 
 
 export default class App extends Component {
