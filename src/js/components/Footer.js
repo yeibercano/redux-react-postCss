8cd36ad448a5927841cadf6a46d24e52938b57c2
Yeiber Cano <yeibercano@gmail.com>
2016-06-28 17:44:35 -0700

    add class to component

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
   render() {
     return (
-     <footer className='footer'>
+     <footer className='container'>
        <h1>I'm the footer</h1>
      </footer>
     )
