3b0d9b6c9a7847c28fa520e1b4a8492c428d0ec8
Yeiber Cano <yeibercano@gmail.com>
2016-06-29 23:46:38 -0700

    refactor component and divs

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
   render() {
     return (
-     <footer className='container'>
-       <h1>I'm the footer</h1>
+     <footer className='footer '>
+       <div className='container'>
+         <h1>I'm the footer</h1>
+         </div>
      </footer>
     )
   }
