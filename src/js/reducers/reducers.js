2dc0788c3d2623a580f727ce5fa22975b2e6c943
Yeiber Cano <yeibercano@gmail.com>
2016-07-03 15:26:47 -0700

    change names

--- src/js/reducers/reducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import { userReducer } from './userReducer.js';
-import { tweetsReducer } from './tweetsReducer.js';
-
-export const reducers = combineReducers({
-  user: userReducer,
-  tweets: tweetsReducer,
-})
