5f61de6d5782684011c5ee95dfa4966186b0b28f
Yeiber Cano <yeibercano@gmail.com>
2016-07-02 23:52:39 -0700

    change file extension

---
+++ src/js/client.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from "redux";
+import { Provider } from 'react-redux';
+import logger from 'redux-logger'
+import { browserHistory } from 'react-router'
+
+import { reducers } from './reducers/reducers.js';
+import App from './containers/App.js';
+
+import styles from '../styles/main.css'
+
+const store = createStore(reducers);
+const history = browserHistory
+
+render(
+  <Provider store={store} history={history}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
+
+store.subscribe(() => {
+  console.log("store changed", store.getState())
+})
+
