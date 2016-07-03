c6319537eb8e933582d8b557b71ad108fda74cba
Yeiber Cano <yeibercano@gmail.com>
2016-07-02 23:53:15 -0700

    change file extension

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+  render() {
+    return (
+     <footer className='footer '>
+       <div className='container'>
+         <h1>I'm the footer</h1>
+         </div>
+     </footer>
+    )
+  }
+}
