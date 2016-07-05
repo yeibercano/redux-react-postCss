21d92c416c25a9714488fc34abf6025e3f6f72a7
Yeiber Cano <yeibercano@gmail.com>
2016-07-04 22:32:05 -0700

    create smart component to retrieve photos

--- src/js/components/MainContent.jsx
+++ src/js/components/MainContent.jsx
@@ -1,9 +1,33 @@
+import { connect } from 'react-redux';
+
 import React from 'react';
+import Photos from './Photos';
+import { getPhotos } from '../actions/index'
 
 class MainContent extends React.Component {
+ 
+  componentWillMount() {
+    console.log('inside component')
+    this.props.dispatch(getPhotos())
+  }
+
   render() {
     return (
-        <MainApp {...this.props}/>
+      <main className='container'>
+        <div className='mainTitle'>Vote your favorite</div>
+        <Photos allImg = {this.props.allImg} />
+      </main>
     );
   }
 }
+
+function mapStateToProps (state) {
+  console.log('state mapTo',state)
+  const allImg = state.list;
+  return {
+    allImg
+  }
+
+}
+
+export default connect(mapStateToProps)(MainContent)
