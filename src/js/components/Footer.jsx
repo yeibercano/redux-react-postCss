659430437035549091886e485715c6148fa4c174
Yeiber Cano <yeibercano@gmail.com>
2016-07-05 02:41:25 -0700

    refactor into functional stateless component

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 
-const Footer = () => {
-  return (
-     <footer className='footer '>
-       <div className='container'>
-         <h1>I'm the footer</h1>
-       </div>
-     </footer>
-    )
-}
+const Footer = () => (
+ 
+	<footer className='footer'>
+	  <div className='container'>
+		  <h1>Footer 2016</h1>
+		</div>
+	</footer>
+)
 
 export default Footer
