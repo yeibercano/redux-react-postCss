9ce79371baaed988a90047db87f99fec1168c99b
Yeiber Cano <yeibercano@gmail.com>
2016-07-04 22:59:21 -0700

    refactor header

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -4,9 +4,16 @@ export default class Header extends Component {
   render() {
     return (
      <header>
-      <div className = "container">
-       <h1>I'm the header</h1>
-      </div>
+       <div className='header-wrapper container'>
+	       <div className="brand">BRAND</div>
+	       <nav >
+	        <ul>
+	          <li><a href="#" title="home">home</a></li>
+	          <li><a href="#" title="blog">blog</a></li>
+	          <li><a href="#" title="categories">categories</a></li>
+	        </ul>
+	       </nav>
+       </div>
      </header>
     )
   }
