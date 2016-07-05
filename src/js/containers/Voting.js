b0954187e0a82ff50638cca9a7802929b251b36d
Yeiber Cano <yeibercano@gmail.com>
2016-07-04 23:06:55 -0700

    code clean up

--- src/js/containers/Voting.js
+++ src/js/containers/Voting.js
@@ -6,24 +6,22 @@ import { getPhotos, upVote } from '../actions/index'
 class Voting extends React.Component {
   render() {
     return (
-        <section>
-          <aside>
-            <button onClick = { e => {
-                 e.preventDefault()
-                 console.log('clicked',this.props.imageId)
-                 upVote(this.props.imageId)
-               
+      <section>
+        <aside>
+          <button onClick = { e => {
+             e.preventDefault()
+             console.log('clicked',this.props.imageId)
+             upVote(this.props.imageId)
              }} 
              value="Vote"> Vote
-            </button>
-          </aside>
-          <aside className = "upVote">
-            {this.props.imageInfo.vote}
-          </aside>
-        </section>
+          </button>
+        </aside>
+        <aside className = "upVote">
+          {this.props.imageInfo.vote}
+        </aside>
+      </section>
     );
   }
 }
-
 
 export default Voting
