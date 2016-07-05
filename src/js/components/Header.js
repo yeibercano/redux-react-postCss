6f65534470538e87b0f8cf06cf7eb490fa6d1b84
Yeiber Cano <yeibercano@gmail.com>
2016-07-05 02:40:29 -0700

    refactor into functional stateless

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 
-export default class Header extends Component {
-  render() {
-    return (
-     <header>
-       <div className='header-wrapper container'>
-	       <div className="brand">BRAND</div>
-	       <nav >
-	        <ul>
-	          <li><a href="#" title="home">home</a></li>
-	          <li><a href="#" title="blog">blog</a></li>
-	          <li><a href="#" title="categories">categories</a></li>
-	        </ul>
-	       </nav>
-       </div>
-     </header>
-    )
-  }
-}
+const Header = () => (
+	<header>
+	 <div className='header-wrapper container'>
+	   <div className="brand">BRAND</div>
+	   <nav >
+	    <ul>
+	      <li><a href="#" title="home">home</a></li>
+	      <li><a href="#" title="blog">blog</a></li>
+	      <li><a href="#" title="categories">categories</a></li>
+	    </ul>
+	   </nav>
+	 </div>
+	</header>
+)
+
+export default Header
