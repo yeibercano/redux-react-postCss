70a8bfa4b9806a686690412efbdbe5a1712443c2
Yeiber Cano <yeibercano@gmail.com>
2016-07-05 23:40:17 -0700

    inject upVote action

--- src/js/reducers/reducers.jsx
+++ src/js/reducers/reducers.jsx
@@ -1,11 +1,20 @@
 import * as types from '../constants/ActionTypes'
 
 export default function appState (state = { list:[] }, action) {
+	console.log('state in reducer', state.list[0])
   switch (action.type) {
     case types.RECEIVE_PHOTOS: {
       state = { ...state, 
           list: action.payload }
     }
+    case types.UP_VOTE: 
+      if (state.list[0] === action.id) {
+      	console.log('hola')
+	      return Object.assign({}, state.list[0], {
+	        vote: state.list[0]++
+	      })
+      }
+
     default:
       return state;
   }
