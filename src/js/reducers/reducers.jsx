c2e7340aa0678af28bd574e463e4a5c822a26161
Yeiber Cano <yeibercano@gmail.com>
2016-07-08 21:32:13 -0700

    work on upVote Reducer

--- src/js/reducers/reducers.jsx
+++ src/js/reducers/reducers.jsx
@@ -8,13 +8,18 @@ export default function appState (state = { list:[] }, action) {
           list: action.payload }
     }
     case types.UP_VOTE: 
-      if (state.list[0] === action.id) {
-      	console.log('hola')
-	      return Object.assign({}, state.list[0], {
-	        vote: state.list[0]++
-	      })
-      }
-
+    console.log('inside reducer upvote')
+      return Object.assign({}, state, {
+      	list: state.list.map((image) => {
+      		console.log('iterating through images')
+      		if(image.id === action.id ) {
+      			return Object.assign({}, list, {
+      				vote: image.vote++
+      			})
+      		}
+      		return image
+      	})
+      })
     default:
       return state;
   }
