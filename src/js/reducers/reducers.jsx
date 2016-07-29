940ff4d78761d70181ce735e4ff00dfa1d23022d
Yeiber Cano <yeibercano@gmail.com>
2016-07-28 23:25:55 -0700

    refactor upVote case

--- src/js/reducers/reducers.jsx
+++ src/js/reducers/reducers.jsx
@@ -1,25 +1,17 @@
 import * as types from '../constants/ActionTypes'
 
-export default function appState (state = { list:[] }, action) {
-	console.log('state in reducer', state.list[0])
+export default function appState(state = { list:[] }, action) {
   switch (action.type) {
-    case types.RECEIVE_PHOTOS: {
-      state = { ...state, 
+    case types.RECEIVE_PHOTOS: 
+    return  { ...state, 
           list: action.payload }
-    }
     case types.UP_VOTE: 
-    console.log('inside reducer upvote')
-      return Object.assign({}, state, {
-      	list: state.list.map((image) => {
-      		console.log('iterating through images')
-      		if(image.id === action.id ) {
-      			return Object.assign({}, list, {
-      				vote: image.vote++
-      			})
-      		}
-      		return image
-      	})
-      })
+      return {...state, list: state.list.map(function(img){
+        if(img.id === action.payload){
+           img.fileattributes.vote++
+        }
+        return img
+      })}
     default:
       return state;
   }
