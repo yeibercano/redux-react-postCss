04ffa3b62d423f5a9676f809c7b6be4dc9635d16
Yeiber Cano <yeibercano@gmail.com>
2016-07-28 23:56:20 -0700

    code cleanup

--- src/js/components/Photos.jsx
+++ src/js/components/Photos.jsx
@@ -6,36 +6,29 @@ import { upVote } from '../actions/index'
 class Photos extends Component {
 
   imageInfo(imageInfo, id) {
-    // console.log('imageInfo, id', imageInfo, id)
     return (
       <article className='photo-list-article' key={imageInfo.id}>
         <h2 className='photo-list-title'>{imageInfo.title}</h2>
         <a href="#fullSize">
           <img className='photo-list-images' src={'../images/' + imageInfo.filename} /> 
         </a>
-
         <div id="fullSize" className="fullSize">
-         
             <a href="#close" title="Close" className="close">X</a>
             <img src={'../images/' + imageInfo.filename} /> 
-          
         </div>
         <p className="movies-description">{imageInfo.synopsis}</p>
         <Voting imageInfo = {imageInfo} imageId={id} />
       </article>
-      // make this into photo-items component that renders the voting as well.
-      // the login comes from main
-      // should have click handler dispatch action to main 
+
     )
   }
   render() {
-    console.log('inside Photos', this.props.allImg)
     return (
-        <div className='photo-list-wrapper'>
-          <section className='photo-list'>
-             {this.props.allImg.map(image => this.imageInfo(image.fileattributes, image.id))}
-          </section>
-        </div>
+      <div className='photo-list-wrapper'>
+        <section className='photo-list'>
+          {this.props.allImg.map(image => this.imageInfo(image.fileattributes, image.id))}
+        </section>
+      </div>
     );
   }
 }
