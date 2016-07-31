49934814e07e295bf583b267fc76d080c68c0906
Yeiber Cano <yeibercano@gmail.com>
2016-07-30 23:48:15 -0700

    code cleanup

--- src/js/reducers/reducers.jsx
+++ src/js/reducers/reducers.jsx
@@ -3,8 +3,8 @@ import * as types from '../constants/ActionTypes'
 export default function appState(state = { list:[] }, action) {
   switch (action.type) {
     case types.RECEIVE_PHOTOS: 
-    return  { ...state, 
-          list: action.payload }
+      return  { ...state, 
+        list: action.payload }
     case types.UP_VOTE: 
       return {...state, list: state.list.map(function(img){
         if(img.id === action.payload){
